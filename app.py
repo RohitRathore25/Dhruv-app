af57d1f349776f0b4d62dcfe211b62ab1cef96d0
RohitRathore25 <singhrathore00008@gmail.com>
2026-01-17 23:41:19 +0530

    push 2

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 
 st.set_page_config(
-    page_title="Rai Properties Prototype",
+    page_title="Rai Properties",
     layout="wide"
 )
 
-st.title("🏢 Rai Properties – Prototype")
+st.title("🏢 Rai Properties")
 
 @st.cache_data
 def load_data():
@@ -14,6 +14,46 @@ def load_data():
 
 df = load_data()
 
-st.success("Data loaded successfully ✅")
+st.success("Data loaded successfully")
 st.write("Preview of data:")
 st.dataframe(df.head(), use_container_width=True)
+
+
+st.set_page_config(
+    page_title="Rai Properties",
+    layout="wide"
+)
+
+st.title("🏢 Rai Properties Dashboard")
+
+@st.cache_data
+def load_data():
+    return pd.read_csv("D:\Sagarpur Project\Rai_Properties_Dataset.csv")
+
+df = load_data()
+
+st.success("Data loaded successfully")
+
+
+# Dashboard
+
+total_properties = df["property_id"].nunique()
+total_units = df["unit_id"].nunique()
+
+occupied_units = df[df["unit_status"] == "OCCUPIED"]["unit_id"].nunique()
+empty_units = df[df["unit_status"] == "EMPTY"]["unit_id"].nunique()
+
+monthly_rent = df["rent_amount_paid"].fillna(0).sum()
+
+col1, col2, col3, col4, col5 = st.columns(5)
+
+col1.metric("Total Properties", total_properties)
+col2.metric("Total Units", total_units)
+col3.metric("Occupied Units", occupied_units)
+col4.metric("Empty Units", empty_units)
+col5.metric("Monthly Rent (₹)", int(monthly_rent))
+
+st.divider()
+
+
+
